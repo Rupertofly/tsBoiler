68905c78a49f5ca2df86548db27e38f0de5da055
Rupertofly <rupertqmoore@me.com>
2018-06-01 11:23:01 +1000

    fix xy to index

--- src/lib/helperFuncs.js
+++ src/lib/helperFuncs.js
@@ -37,7 +37,7 @@ function dist2(v, w) {
 function distToSegmentSquared(p, v, w) {
   var l2 = dist2(v, w);
   if (l2 === 0) return dist2(p, v);
-  var t = ((p.x - v[0]) * (w[0] - v[0]) + (p.y - v[1]) * (w[1] - v[1])) / l2;
+  var t = ((p[0] - v[0]) * (w[0] - v[0]) + (p[1] - v[1]) * (w[1] - v[1])) / l2;
   t = Math.max(0, Math.min(1, t));
   return dist2(p, [v[0] + t * (w[0] - v[0]), v[1] + t * (w[1] - v[1])]);
 }
